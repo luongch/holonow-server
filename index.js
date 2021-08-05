b83b5a780eb792b77054a8a0236605cedcf793eb
luongch <christopher_luong@Live.ca>
2021-08-04 21:52:58 -0400

    update video list logic to return list of video objects instead of only the ids

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const videoListApi = require('./search/video-list-api')
 const videoInfoApi = require('./search/video-info-api')
+const express = require('express')
+const app = express();
 
 const main = async() => {
+    app.get('/', (req,res)=>{
+        res.send('we are on home')
+    });
+    app.get('/videoList', (req,res)=>{
+        res.send(videoList)
+    });
+    app.get('/videoInfo', (req,res)=>{
+        res.send(videoInfo.data.items)
+    });
+    app.listen(3000)
     // get all the videos latest videos for each channel    
     let videoList = await videoListApi();
 
--- search/video-info-api.js
+++ search/video-info-api.js
@@ -5,11 +5,15 @@ const {google} = require('googleapis');
 module.exports = async (videoList) => {
     console.log('video info api start');
     // loop through list of videos to do a batch call to videos api
+    let videoIdList = [];
+    videoList.forEach(video => {
+        videoIdList.push(video.id)
+    });
     
     let promise = google.youtube('v3').videos.list({
         key: process.env.YOUTUBE_TOKEN,
         part: 'liveStreamingDetails',
-        id: videoList.join()
+        id: videoIdList
     })
     
     return promise;
